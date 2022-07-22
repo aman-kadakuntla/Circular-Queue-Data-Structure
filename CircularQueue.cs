727b96a213287fb53f9690f9eba96acf963912ff
Aman11 <kadakuntlaaman@gmail.com>
2022-07-22 17:16:46 +0530

    implemented circular queue using c#

---
+++ CircularQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Queue
+{
+    internal class CircularQueue
+    { 
+        int[] arr;
+        int front, rear, maxSize;
+        public CircularQueue(int maxSize)
+        {
+            arr = new int[maxSize];
+            front = -1;
+            rear = -1;
+            this.maxSize = maxSize;
+        }
+        public bool Add(int val)
+        {
+            if(front==-1 && rear==-1)
+            {
+                front++;
+                rear++;
+                arr[front] = val;
+            }
+            else 
+            {
+                if (front == ((rear + 1) % maxSize))
+                {
+                    return false;
+                }
+                rear = (rear + 1) % maxSize;
+                arr[rear] = val;
+            }
+            return true;
+        }
+        public bool Delete()
+        {
+            
+            if (front == maxSize - 1 && rear== maxSize-1)
+            {
+                front = -1; rear = -1;
+            }
+            else if(front==-1&& rear==-1)
+            {
+                return false;
+            }
+            else if (front == rear)
+            {
+                front = -1; rear = -1;
+            }
+            else 
+            {
+                front=(front + 1)%maxSize;
+            }
+            return true;
+        }
+        public List<int> display()
+        {
+            List<int> list = new List<int>();
+            if (front == -1 || rear == -1)
+                return list;
+            int i = front;
+            while(i!=rear)
+            {
+                list.Add(arr[i]);
+                i = (i + 1) % maxSize;
+            }
+            list.Add(arr[rear]);
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select option");
+            CircularQueue queue = new CircularQueue(3);
+        label:
+            Console.WriteLine("1. Add\n2. Delete\n3. Display\n4. Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter number");
+                    if(queue.Add(Convert.ToInt32(Console.ReadLine())))
+                    {
+                        Console.WriteLine("Added successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue might be full");
+                    }
+                    goto label;
+                case 2:
+                    if (queue.Delete())
+                        Console.WriteLine("Element deleted");
+                    else
+                        Console.WriteLine("Queue is empty");
+                    goto label;
+                case 3:
+                    if (queue.display().Count == 0)
+                        Console.WriteLine("Queue is empty");
+                    else
+                    {
+                        Console.Write("Queue elements : ");
+                        foreach (var item in queue.display())
+                            Console.Write(item + " ");
+                        Console.WriteLine();
+                    }
+                    goto label;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    goto label;
+
+            }
+        }
+    }
+}
